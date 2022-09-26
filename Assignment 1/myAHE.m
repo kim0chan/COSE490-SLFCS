f70fb8b567902e03f775fb5562498ee55153216b
kim0chan <39790536+kim0chan@users.noreply.github.com>
2022-09-26 21:59:35 +0900

    progressing on myAHE.m

--- Assignment 1/myAHE.m
+++ Assignment 1/myAHE.m
@@ -7,5 +7,31 @@ output = uint8(zeros(dimX,dimY));
 
 % ToDo
 
+stepSizeX = ceil(dimX / numtiles(1));
+stepSizeY = ceil(dimY / numtiles(2));
+
+localHistogram = uint8(zeros(dimX, dimY));
+
+% Local Histogram Section
+for i = 1 : numtiles(1)
+    startX = (i - 1) * stepSizeX + 1;
+    endX = startX + stepSizeX - 1;
+    if dimX < endX
+            endX = dimX;
+    end
+    for j = 1 : numtiles(2)
+        startY = (j - 1) * stepSizeY + 1;
+        endY = startY + stepSizeY - 1;
+        if dimY < endY
+            endY = dimY;
+        end
+        
+        tempImage = input(startX:endX, startY:endY);
+        localHistogram(startX:endX, startY:endY) = myHE(tempImage);
+        
+    end
+end
+
+
 
 end
--- Assignment 1/myCDF.m
+++ Assignment 1/myCDF.m
@@ -10,11 +10,11 @@ dimY = size(image, 2);
 histogram=zeros(256, 1);
 for i = 1 : dimX
     for j = 1 : dimY
-        histogram(image(i, j)) = histogram(image(i, j)) + 1;
+        histogram(image(i, j) + 1) = histogram(image(i, j) + 1) + 1;
     end
 end
 
-output(1) = 255*histogram(1)/(dimX * dimY);
+output(1) = 255*histogram(1)/(dimX * dimY) - 1;
 
 for i = 2 : 256
     output(i) = output(i-1) + 255*histogram(i)/(dimX * dimY);
