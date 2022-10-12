7cf06c9e98528beaee7c2fdb70a0830b8097377e
kim0chan <39790536+kim0chan@users.noreply.github.com>
2022-10-12 13:37:15 +0900

    Notch filter ver.2 / automation needed

--- Assignment 2/myNotch.m
+++ Assignment 2/myNotch.m
@@ -25,266 +25,300 @@ figure,imshow(log(1+abs((F))), []);
 %
 
 % Parameters
-n = 4;
+n = 3;
 D_0 = 40;
 
 % Generating 1 + k*[1 - H_LP(u, v)]
 % Hard-coding section.
-H = ones(PQ(1), PQ(2));
+H = zeros(PQ(1), PQ(2));
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
-        D = sqrt((i - 130)^2 + (j - 832)^2);
+        D = sqrt((i - 130)^2 + (j - 830)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
-        D = sqrt((i - 554)^2 + (j - 832)^2);
+        D = sqrt((i - 554)^2 + (j - 830)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
-        D = sqrt((i - 130)^2 + (j - 192)^2);
+        D = sqrt((i - 130)^2 + (j - 194)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
-        D = sqrt((i - 554)^2 + (j - 192)^2);
+        D = sqrt((i - 554)^2 + (j - 194)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 84)^2 + (j - 513)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 601)^2 + (j - 513)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 343)^2 + (j - 126)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 343)^2 + (j - 898)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 84)^2 + (j - 126)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 600)^2 + (j - 126)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 600)^2 + (j - 898)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 84)^2 + (j - 898)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 130)^2 + (j - 58)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 554)^2 + (j - 58)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 554)^2 + (j - 966)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 130)^2 + (j - 966)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 176)^2 + (j - 126)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 508)^2 + (j - 126)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 508)^2 + (j - 898)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 176)^2 + (j - 898)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 40)^2 + (j - 194)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 644)^2 + (j - 194)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 40)^2 + (j - 830)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 644)^2 + (j - 830)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 298)^2 + (j - 194)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 386)^2 + (j - 830)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 298)^2 + (j - 830)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 386)^2 + (j - 194)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 130)^2 + (j - 582)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 554)^2 + (j - 442)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 130)^2 + (j - 442)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 554)^2 + (j - 582)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 176)^2 + (j - 512)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 for i = 1 : PQ(1)
     for j = 1 : PQ(2)
         D = sqrt((i - 508)^2 + (j - 512)^2);
         H_LP_uv = 1 / (1 + (D / D_0) ^ (2*n));
-        H(i, j) = H(i,j)-H_LP_uv;
+        H(i, j) = 1-H_LP_uv;
     end
 end
+F = F .* H;
 
 
 % Generating F
