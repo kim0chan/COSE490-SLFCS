be796b418bde83a333f5be5d0f8973657ff190a6
kim0chan <39790536+kim0chan@users.noreply.github.com>
2022-12-01 13:05:29 +0900

    assignment 4 colormap까지 구현함

--- Assignment 4/harris_corner.m
+++ Assignment 4/harris_corner.m
@@ -17,8 +17,65 @@ Img=double(I(:,:,1));
 %
 % ToDo: Compute R
 %
-R = ...
+[numRows,numCols] = size(Img);
+[xgrad, ygrad] = gradient(Img);
 
+xxgrad = xgrad .^ 2;
+yygrad = ygrad .^ 2;
+xygrad = xgrad .* ygrad;
+
+f = fspecial('gaussian',5,1.0);
+xgrad_smth = imfilter(xxgrad, f, 'symmetric');
+ygrad_smth = imfilter(yygrad, f, 'symmetric');
+xygrad_smth = imfilter(xygrad, f, 'symmetric');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+H = zeros(2, 2, numRows, numCols);
+R = zeros(numRows, numCols);
+for i = 1: numRows
+    for j = 1: numCols
+        H(1, 1, i, j) = xgrad_smth(i, j);
+        H(1, 2, i, j) = xygrad_smth(i, j);
+        H(2, 1, i, j) = xygrad_smth(i, j);
+        H(2, 2, i, j) = ygrad_smth(i, j);
+    end
+end
+
+%{
+H = zeros(2, 2);
+H(1, 1) = sum(xgrad_2, 'all');
+H(1, 2) = sum(xygrad, 'all');
+H(2, 1) = sum(xygrad, 'all');
+H(2, 2) = sum(ygrad_2, 'all');
+
+k = 0.5;
+det_H = det(H);
+trace_H = trace(H);
+
+R = det_H - k * trace_H^2;
+%}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+k = 0.03;
+sum = 0;
+for i = 1: numRows
+    for j = 1: numCols
+        det_H = det(H(:, :, i, j));
+        trace_H  = trace(H(:, :, i, j));
+        R(i, j) = det_H - k * (trace_H^2);
+        sum = sum + R(i, j);
+    end
+end
+avg = sum / (numRows * numCols);
+
+for i = 1: numRows
+    for j = 1: numCols
+        det_H = det(H(:, :, i, j));
+        trace_H  = trace(H(:, :, i, j));
+        R(i, j) = det_H - k * (trace_H^2);
+        sum = sum + R(i, j);
+    end
+end
 
 %
 % Example of collecting points and plot them
@@ -29,15 +86,14 @@ location = [10 15 20; 1 2 3]'
 points = cornerPoints(location)
 plot(points)
 
-
 %
 % ToDo: Visualize R values using jet colormap
 %
 
-...
-
-
-
+imshow(R, []);
+c = jet;
+colormap(c);
+colorbar;
 
 %
 % ToDo: Threshold R & Collect Local Maximum Points
@@ -49,7 +105,7 @@ plot(points)
 %
 % Visualize corner points over the input image
 %
-
+%{
 imshow(I)
 
 hold on
@@ -57,4 +113,4 @@ hold on
 plot(points)
 
 hold off
-
+%}
